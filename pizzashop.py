d3569d3150be7cb2e19dd0916596b9dd29d044a8
nikn3s <88500366+nikn3s@users.noreply.github.com>
2022-12-23 19:16:02 +0000

    Update pizzashop.py

--- pizzashop.py
+++ pizzashop.py
@@ -1,20 +1,27 @@
 def clients(*names):
-    people =list()
-    for name in range(len(names)):
-        people.append(name)
-    return tuple(people) 
+    # illiterate through the array of people
+    people = (name for name in names)   
+    return tuple(people)
+
 try:
     count = int(input("How many clients?: ")) 
 except ValueError:
     print("Please enter a number")
+    exit()
+# create a file to store names
+names = tuple()
+for i in range(count):
+    name = input("Enter client name: ")
+    # add each name to the array
+    names += clients(name)
 
-for person in range(count):
-    dude = input("Name:")
-    print(clients(dude))
+print(names)
 
 # 22/12/2022 - Bug: The tuple 'people' returns a tuple with integer 0. I will fix this later
 #How many clients?: 2
 #Name:Alex
 # (0,)
 # Name:Gorgia 
-# (0,)
+# (0,) 
+
+# 23/12/2022 - Everthing now works just fine; the function was easier to make than I thought
